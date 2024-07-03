c31bcc37d7ba0bac7e3258a89c030d82e5bc6979
gabrielaalmeida0904 <gabrielaalmeida0904@gmail.com>
2024-07-02 21:01:42 -0300

    Add files via upload

---
+++ EX05.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        // Solicita o valor da base do retângulo ao usuário
+        Console.WriteLine("Digite o valor da base do retângulo:");
+        double valorBase = double.Parse(Console.ReadLine());
+
+        // Solicita o valor da altura do retângulo ao usuário
+        Console.WriteLine("Digite o valor da altura do retângulo:");
+        double valorAltura = double.Parse(Console.ReadLine());
+
+        // Calcula a área do retângulo
+        double areaRetangulo = valorBase * valorAltura;
+
+        // Exibe a área calculada
+        Console.WriteLine($"A área do retângulo é: {areaRetangulo}");
+
+        // Verifica se a área do retângulo é maior que 100 e exibe uma mensagem correspondente
+        if (areaRetangulo > 100)
+        {
+            Console.WriteLine("Terreno grande");
+        }
+        else
+        {
+            Console.WriteLine("Terreno pequeno");
+        }
+    }
+}
